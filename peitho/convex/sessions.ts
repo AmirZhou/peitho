daf0b4f960685a021f194468c5316f6c623a47f7
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-18 01:03:36 -0700

    checkpoint: modify peitho/convex/sessions.ts - 01:03

--- peitho/convex/sessions.ts
+++ peitho/convex/sessions.ts
@@ -1,6 +1,22 @@
 import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
 
+// Generate upload URL for recording
+export const generateUploadUrl = mutation({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.storage.generateUploadUrl();
+  },
+});
+
+// Get recording URL by storage ID
+export const getRecordingUrl = query({
+  args: { storageId: v.id("_storage") },
+  handler: async (ctx, args) => {
+    return await ctx.storage.getUrl(args.storageId);
+  },
+});
+
 // Get all sessions
 export const list = query({
   args: {},
